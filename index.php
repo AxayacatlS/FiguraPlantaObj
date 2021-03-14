4cd5c1209c773d83ed0f6f8fa2d656e447462a18
AxayacatlS <38516115+AxayacatlS@users.noreply.github.com>
2021-03-14 16:55:20 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     </a-assets>-->
     <a-scene embedded arjs>
       <a-entity scale=".2 .2 .2"> 
-        <a-entity gltf-model="plantacolor.glb" scale="5 5 5" rotation="180 0 0" crossOrigin="anonymous"></a-entity>
+        <a-entity gltf-model="plantacolor.glb" scale="5 5 5" rotation="0 0 180" crossOrigin="anonymous"></a-entity>
         <!--<a-entity obj-model="obj: #obj;" scale="1 1 1" crossOrigin="anonymous"></a-entity>-->
         <a-marker-camera preset='hiro'></a-marker-camera>
       </a-entity>
